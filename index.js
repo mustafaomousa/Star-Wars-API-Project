61de587646f76305a94eeb24e83f8a89bdc250c3
Mustafa <mustafaomousa@icloud.com>
2020-10-13 17:01:41 -0500

    My final masterpiece

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 const fetch = require(`node-fetch`);
 const fs = require('fs').promises
 
-const person = "https://swapi.dev/api/people/3";
+const person = "https://swapi.dev/api/people/1";
 let personName = '';
-
-const globalMovieArray = [];
+let newFileContents = '';
+let titles = [];
 
 fetch(person)
     .then(response => response.json())
     .then(person => {
-        // console.log(`${person.name}\n`)
-        personName = person.name;
+        personName = getPersonName(person)
         return fetch(person.homeworld);
     })
     .then(response2 => response2.json())
     .then(homeworld => {
-        const filmArray = homeworld.films
-        filmArray.map(film => {
-            globalMovieArray.push(film);
-        })
-        let movieRequests = globalMovieArray.map(movie => fetch(movie));
+        const filmArray = createFilmArray(homeworld)
+        let movieRequests = filmArray.map(movie => fetch(movie));
         Promise.all(movieRequests)
             .then(responses => {
                 Promise.all(responses.map(res => res.json()))
                 .then(films => {
-                    console.log(`\n${personName} has starred in:\n`)
-                    for(let film of films) {
-                    console.log(film.title)
-                    }
+                    addIntroToFileContents(homeworld, films);
+                    createFile();
                 })
             })
+  })
 
-  
 
+  function addIntroToFileContents (homeworld, films) {
+      newFileContents += `My name is ${personName}, I am from ${getPersonLocation(homeworld)}
+I starred in the following films: `
+      for (let film of films) {
+          titles.push(' ' + film.title)
+      }
+      newFileContents += `${titles.toString(`,`)}.`
+  }
 
-    // .then(homeworld => fetch(homeworld.films))
-    // .then(response3 => response3.json())
-    // .then(films => console.log(films))
-    // .then(homeworld => console.log(json))
-    // .then(response3 => response3.json())
-    // .then(films => {
-    //     console.log(films)
-    })
+  function createFile () {
+      fs.writeFile('character.txt', newFileContents, 'utf-8')
+          .then(() => console.log("Your requested info has been turned into a file"))
+          .catch(e => console.log("You've encountered the following error:\n\n " + e))
+  }
+
+  function getPersonName (person) {
+      return person.name;
+  }
+
+  function getPersonLocation (homeworld) {
+      return homeworld.name;
+  }
+
+  function createFilmArray (homeworld) {
+    return homeworld.films;
+  }
