9cbc22b75d711b6b8ff2c996768129230b84ff07
Mustafa <mustafaomousa@icloud.com>
2020-10-13 15:57:20 -0500

    I'm a wittttle confused

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fetch = require(`node-fetch`);
 
 const person = "https://swapi.dev/api/people/1";
 
-const movieArray = [];
+const globalMovieArray = [];
 
 fetch(person)
     .then(response => response.json())
@@ -11,4 +11,29 @@ fetch(person)
         return fetch(person.homeworld);
     })
     .then(response2 => response2.json())
-    .then(homeworld => console.log(homeworld))
+    .then(homeworld => {
+        const filmArray = homeworld.films
+        filmArray.map(film => {
+            globalMovieArray.push(film);
+        })
+        let movieRequests = globalMovieArray.map(movie => fetch(movie));
+        Promise.all(movieRequests)
+            .then(responses => {
+                for(let response of responses){
+                    return fetch(response);
+                }
+            })
+            .then(movieResponse => movieResponse.json())
+            .then(movie => console.log(movie));
+
+  
+
+
+    // .then(homeworld => fetch(homeworld.films))
+    // .then(response3 => response3.json())
+    // .then(films => console.log(films))
+    // .then(homeworld => console.log(json))
+    // .then(response3 => response3.json())
+    // .then(films => {
+    //     console.log(films)
+    })
