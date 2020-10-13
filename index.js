012681c1a5e3fc457a0c4b52f4cf815ae66ddf00
Mustafa <mustafaomousa@icloud.com>
2020-10-13 16:14:40 -0500

    Wow my code looks good

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const fetch = require(`node-fetch`);
 const fs = require('fs').promises
 
-const person = "https://swapi.dev/api/people/1";
+const person = "https://swapi.dev/api/people/3";
+let personName = '';
 
 const globalMovieArray = [];
 
 fetch(person)
     .then(response => response.json())
     .then(person => {
-        console.log(`${person.name}\n`)
+        // console.log(`${person.name}\n`)
+        personName = person.name;
         return fetch(person.homeworld);
     })
     .then(response2 => response2.json())
@@ -21,10 +23,13 @@ fetch(person)
         Promise.all(movieRequests)
             .then(responses => {
                 Promise.all(responses.map(res => res.json()))
-                .then(film => {console.log(film)})
+                .then(films => {
+                    console.log(`\n${personName} has starred in:\n`)
+                    for(let film of films) {
+                    console.log(film.title)
+                    }
+                })
             })
-            .then(movieResponse => movieResponse.json())
-            .then(movie => console.log(movie));
 
   
 
