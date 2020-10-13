43f9d7e009b20dcbf00d481fcff812127812b03c
Mustafa <mustafaomousa@icloud.com>
2020-10-13 15:18:35 -0500

    Here's a better way to get person and homeworld

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const fetch = require(`node-fetch`);
 
 const person = "https://swapi.dev/api/people/1";
 
+const movieArray = [];
+
 fetch(person)
     .then(response => response.json())
     .then(person => {
         console.log(`${person.name}\n`)
-        const homeworldUrl = person.homeworld;
-        fetch(homeworldUrl)
-            .then(response2 => response2.json())
-            .then(homeworld => console.log(homeworld))
+        return fetch(person.homeworld);
     })
-    .then()
+    .then(response2 => response2.json())
+    .then(homeworld => console.log(homeworld))
